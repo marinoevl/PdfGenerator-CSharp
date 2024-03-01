6a26e6ff2b22ea94bbb4ccb2d9d89229bcb24efb
Marino Villar <marinovillarlara@gmail.com>
2024-03-01 14:31:34 -0400

    UnitTesting

--- tests/UnitTests/App.Templates.UnitTests/Create/CreateTemplateCommandHandlerUnitTests.cs
+++ tests/UnitTests/App.Templates.UnitTests/Create/CreateTemplateCommandHandlerUnitTests.cs
@@ -1,4 +1,7 @@
+using System.Linq.Expressions;
+using App.Shared.Exceptions;
 using App.Templates.Create;
+using LinqKit;
 using PdfGenerator.Domain.Shared;
 using PdfGenerator.Domain.Templates;
 
@@ -8,24 +11,52 @@ public class CreateTemplateCommandHandlerUnitTests
 {
     private readonly Mock<IGenericRepository<Template, Guid>> _mockTemplateRepository;
     private readonly Mock<IUnitOfWork> _mockUnitOfWork;
-    private readonly CreateTemplateCommandHandler _handler;
+    private CreateTemplateCommandHandler _handler;
 
     public CreateTemplateCommandHandlerUnitTests()
     {
         _mockTemplateRepository = new Mock<IGenericRepository<Template, Guid>>();
         _mockUnitOfWork = new Mock<IUnitOfWork>();
-        _handler = new CreateTemplateCommandHandler(_mockUnitOfWork.Object, _mockTemplateRepository.Object);
     }
 
     [Fact]
-    public void HandlerCreateTemplate_WhenTemplateContentIsEmpty()
+    public async Task HandlerCreateTemplate_WhenTemplateNameExists_ShouldRaiseValidationException()
     {
-        
+        //Arrange
+        var template = new Template(Guid.NewGuid(), "template-1", "<httml></html>");
+        var command = new CreateTemplateCommand("template-1", "<httml></html>");
+        var cancelationToken = new CancellationToken();
+        _mockTemplateRepository
+            .Setup(m => m.GetAsync(It.IsAny<Expression<Func<Template, bool>>>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(template);
+        _handler = new CreateTemplateCommandHandler(_mockUnitOfWork.Object, _mockTemplateRepository.Object);
+
+
+        //Act
+        var ex = await Assert.ThrowsAsync<ValidationException>(() => _handler.Handle(command, default));
+
+        //Assert
+        Assert.NotNull(ex);
+        Assert.IsType<ValidationException>(ex);
     }
-    
+
     [Fact]
-    public void HandlerCreateTemplate_When_TemplateNameExists()
+    public async Task HandlerCreateTemplate_WhenItsCorrect_ShouldCreateRecord()
     {
-        
+        //Arrange
+        var template = new Template(Guid.NewGuid(), "template-1", "<httml></html>");
+        var command = new CreateTemplateCommand("template-2", "<httml></html>");
+        var cancelationToken = new CancellationToken();
+        _mockTemplateRepository
+            .Setup(m => m.GetAsync(It.Is<Expression<Func<Template, bool>>>(m => m.Name == template.Name),
+                It.IsAny<CancellationToken>())).ReturnsAsync(template);
+        _handler = new CreateTemplateCommandHandler(_mockUnitOfWork.Object, _mockTemplateRepository.Object);
+
+        //Act
+        var result = await _handler.Handle(command, cancelationToken);
+
+        //Assert
+        _mockTemplateRepository.Verify(r => r.AddAsync(It.IsAny<Template>(), cancelationToken), Times.Once);
+        _mockUnitOfWork.Verify(u => u.SaveChangesAsync(cancelationToken), Times.Once);
     }
 }
