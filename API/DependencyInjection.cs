2f1b1049f7849666a7e8911ff0e862e6b5a69c74
Marino Villar <marinovillarlara@gmail.com>
2024-03-01 08:11:56 -0400

    health checks

--- API/DependencyInjection.cs
+++ API/DependencyInjection.cs
@@ -1,10 +1,24 @@
-﻿namespace PdfGenerator_CSharp.API;
+﻿using Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using PdfGenerator_CSharp.API.Infrastructure;
+
+namespace PdfGenerator_CSharp.API;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddWebApi(this IServiceCollection services)
+    public static IServiceCollection AddWebServices(this IServiceCollection services)
     {
         services.AddControllers();
+        
+        services.AddHealthChecks()
+            .AddDbContextCheck<DefaultDbContext>();
+        
+        services.AddExceptionHandler<CustomExceptionHandler>();
+        
+        // Customise default API behaviour
+        services.Configure<ApiBehaviorOptions>(options =>
+            options.SuppressModelStateInvalidFilter = true);
+        
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
         return services;
--- API/Program.cs
+++ API/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddWebApi()
+builder.Services.AddWebServices()
     .AddInfrastructure(builder.Configuration)
     .AddApplication();
     
